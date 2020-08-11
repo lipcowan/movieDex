978445fabb782662bbc4ad22ae86c12c6a81e502
Lip Cowan <pcowan90@gmail.com>
2020-08-11 13:21:53 -0700

    finished exercise

--- server.js
+++ server.js
@@ -29,21 +29,15 @@ app.get("/movie", function handleGetMovie(req, res) {
   let response = devMovieData;
 
   if (req.query.genre) {
-    response = response.filter((movie) => {
-      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase());
-    });
+    response = response.filter((movie) =>movie.genre.toLowerCase().includes(req.query.genre.toLowerCase()));
   }
 
   if (req.query.country) {
-    response = response.filter((movie) => {
-      movie.country.toLowerCase().includes(req.query.country.toLowerCase());
-    });
+    response = response.filter((movie) =>movie.country.toLowerCase().includes(req.query.country.toLowerCase()));
   }
  
   if (req.query.avg_vote) {
-    response = response.filter((movie) => {
-      Number(movie.avg_vote) >= Number(req.query.avg_vote);
-    });
+    response = response.filter((movie) => Number(movie.avg_vote) >= Number(req.query.avg_vote));
   }
 
 res.json(response);
